ed4a9443382dc8bf4a593659cb3c9a3d273babb4
JM. Hiltbrunner <jm.hiltbrunner@gmail.com>
2018-05-17 10:24:05 +0200

    Factorisation du code par le QuizStateManager

--- src/app/quiz-player/quiz-player.component.ts
+++ src/app/quiz-player/quiz-player.component.ts
@@ -1,47 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { QUIZZES } from '../data/quizzes';
 import { Answer } from '../models/answer';
-import { AnswersState } from '../quiz-state-manager.service';
+import { AnswersState, QuizStateManager } from '../quiz-state-manager.service';
 import { QuizService } from '../quiz.service';
 import { Quiz } from '../models/quiz';
 import { Question } from '../models/question';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-quiz-player',
   templateUrl: './quiz-player.component.html',
-  styles: []
+  styles: [],
+  providers: [QuizStateManager]
 })
 export class QuizPlayerComponent implements OnInit {
 
-  currentQuiz : Quiz;
-  currentQuestion : Question;
-  currentAnswer : Answer;
-  currentAnswers: AnswersState;
+  //Avec un appel aux services, les classes injectées sont observées. Le QSM est la tour de contrôle
+  currentQuiz : Observable<Quiz>;
+  currentQuestion : Observable<Question>;
+  currentAnswer : Observable<Answer>;
+  currentAnswers: Observable<AnswersState>;
 
   isStarted = false;
 
-  constructor(private quizService : QuizService) { 
+  constructor(private quizService : QuizService,
+              private quizStateManager : QuizStateManager) { 
 
   }
 
   ngOnInit() {
+    // Initialise les propriétés du composant à partir du QSM
+    this.currentQuiz= this.quizStateManager.getCurrentQuiz();
+    this.currentQuestion = this.quizStateManager.getCurrentQuestion();
+    this.currentAnswer = this.quizStateManager.getCurrentAnswer();
+    this.currentAnswers = this.quizStateManager.getCurrentAnswers();
+    
     // Charge le quiz que l'utilisateur a cliqué
-    //Astuce : l'id du quiz viendra de l'URL
+    // Astuce : l'id du quiz viendra de l'URL
     const quizId = 32;
+    const quiz = this.quizService.loadQuiz(quizId);
 
-    this.currentQuiz= this.quizService.loadQuiz(quizId);
-    this.currentQuestion = this.currentQuiz.questions[0];
-    this.currentAnswer = new Answer({questionId : this.currentQuestion.id});
-    this.currentAnswers = {}
+    // Pousse le quiz chargé dans l'observable
+    this.quizStateManager.setQuiz(quiz);
+
+
   }
 
-  //Recupère la réponse envoyée par emit et sera utilisée dans l'output dans le code HTML
-  reponseRecue(answer: Answer) {
-    console.log('Enregistrée')
-  }
-
+  
   startQuiz() {
     this.isStarted = true;
+    // TODO: Utilise le QuizStateManager pour aller à la première question
+    this.quizStateManager.gotoFirstQuestion();
+  }
+  //Recupère la réponse envoyée par emit et sera utilisée dans l'output dans le code HTML
+  reponseRecue(answer: Answer) {
+    // vérifie si le programme passe par cette méthode
+    console.log('Enregistrée');
+    // TODO: sauvegarder la reponse dans le service QuizStateManager
+    this.quizStateManager.addAnswer(answer); 
   }
 
+  gotoNextQuestion(){
+    // TODO: Aller à la question suivante avec QuizStateManager.
+    this.quizStateManager.gotoNextQuestion();
+  }
+
+  gotoPreviousQuestion(){
+    // TODO: Aller à la question précédente avec QuizStateManager
+    this.quizStateManager.gotoPreviousQuestion();
+  }
+  
 }
