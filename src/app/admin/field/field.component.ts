41e883d9159430bfb3f8dfe51e1848e0c9b5e68c
JM. Hiltbrunner <jm.hiltbrunner@gmail.com>
2018-05-18 12:13:19 +0200

    Généricité du formulaire

--- src/app/admin/field/field.component.ts
+++ src/app/admin/field/field.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+/**
+ * Cette classe générique permet de générer des champs de textes automatiquement et de contrôler leur validation sans avoir à copier coller
+ */
 
 @Component({
   selector: 'app-field',
@@ -6,6 +11,10 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class FieldComponent implements OnInit {
+
+  @Input() label: string; //quizForm.quizTitle
+  @Input() control: FormControl //quizForm.get('quiztitle')
+  @Input() baliseHTML: string; //<input type="text" class="form-control" placeholder="Titre" formControlName="quizTitle">
 
   constructor() { }
 
